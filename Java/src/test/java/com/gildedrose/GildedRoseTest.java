25b2634444637489a26bd9299979fe35492ba4ed
wujo <wujo635@gmail.com>
2019-03-05 19:45:13 -0500

    Pass Aged Brie quality increases over time

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,23 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
+    private Item[] items;
+    private GildedRose app;
+
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+        items = new Item[]{new Item("foo", 0, 0)};
+        app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    public void shouldIncreaseAgedBrieQualityOnUpdate() {
+        items = new Item[]{new Item("Aged Brie", 1, 1)};
+        app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].quality);
     }
 
 }
