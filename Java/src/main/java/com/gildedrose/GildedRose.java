44dd45d3778882156e81beac5498568e558baa9d
wujo <wujo635@gmail.com>
2019-03-06 23:47:54 -0500

    Fail Backstage pass quality +2 when 5 < sellin < 10

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -23,7 +23,7 @@ class GildedRose {
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                                items[i].quality = items[i].quality + 0;
                             }
                         }
 
