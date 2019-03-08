3f6ab6bb6e42dc7425e84736f7f55d6021d723e7
wujo <wujo635@gmail.com>
2019-03-07 22:37:23 -0500

    Refactor quality out of bounds method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -50,11 +50,6 @@ class GildedRose {
     }
 
     private void correctQualityOutOfBounds(Item item) {
-        if (item.quality < 0) {
-            item.quality = 0;
-        }
-        if (item.quality > 50) {
-            item.quality = 50;
-        }
+        item.quality = Math.min(Math.max(item.quality, 0), 50);
     }
 }
