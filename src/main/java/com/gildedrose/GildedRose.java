2289e45fbfe3eb8a37675eefa7aef4ac91922675
wujo635 <wujo635@gmailc.om>
2019-03-09 13:19:24 -0500

    Refactor item update to separate methods

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,24 +10,36 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (isAgedBrie(item)) {
-                item.quality++;
-                item.sellIn--;
+                updateAgedBrie(item);
             } else if (isBackstagePass(item)) {
-                if (item.sellIn > 0) {
-                    item.quality += 3 - item.sellIn / 5;
-                } else {
-                    item.quality = 0;
-                }
-                item.sellIn--;
+                updateBackstagePass(item);
             } else if (!isSulfuras(item)) {
-                item.quality--;
-                item.sellIn--;
-                if (item.sellIn < 0) {
-                    item.quality--;
-                }
+                updateGenericItem(item);
             }
             correctQualityOutOfBounds(item);
         }
+    }
+
+    private void updateGenericItem(Item item) {
+        item.quality--;
+        item.sellIn--;
+        if (item.sellIn < 0) {
+            item.quality--;
+        }
+    }
+
+    private void updateBackstagePass(Item item) {
+        if (item.sellIn > 0) {
+            item.quality += 3 - item.sellIn / 5;
+        } else {
+            item.quality = 0;
+        }
+        item.sellIn--;
+    }
+
+    private void updateAgedBrie(Item item) {
+        item.quality++;
+        item.sellIn--;
     }
 
     private boolean isSulfuras(Item item) {
