9b2999a2cc66e4b835a5d7332c6b204adc0cfe51
wujo635 <wujo635@gmailc.om>
2019-03-10 18:04:55 -0400

    Refactor tests into set/given/then format

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,96 +9,125 @@ public class GildedRoseTest {
     private Item[] items;
     private GildedRose app;
 
-    private void assertQualitySuccess(int expectedQuality, Item item) {
-        updateAppQuality(item);
-        assertEquals(expectedQuality, app.items[0].quality);
-    }
-
-    private void assertSellInSuccess(int expectedSellIn, Item item) {
-        updateAppQuality(item);
-        assertEquals(expectedSellIn, app.items[0].sellIn);
-    }
-
-    private void updateAppQuality(Item item) {
-        this.items = new Item[]{item};
-        this.app = new GildedRose(items);
-        app.updateQuality();
-    }
-
     @Test
     public void foo() {
-        updateAppQuality(new Item("foo", 0, 0));
+        this.items = new Item[]{(new Item("foo", 0, 0))};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
 
     @Test
     public void shouldIncreaseAgedBrieQualityOnUpdate() {
-        assertQualitySuccess(2, new Item("Aged Brie", 1, 1));
+        this.items = new Item[]{new Item("Aged Brie", 1, 1)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(2, app.items[0].quality);
     }
 
     @Test
     public void shouldDecreaseGeneralItemQualityOnUpdate() {
-        assertQualitySuccess(0, new Item("General Item", 1, 1));
+        this.items = new Item[]{new Item("General Item", 1, 1)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(0, app.items[0].quality);
     }
 
     @Test
     public void shouldNotUpdateSulfurasQuality() {
-        assertQualitySuccess(1, new Item("Sulfuras, Hand of Ragnaros", 1, 1));
+        this.items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 1, 1)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(1, app.items[0].quality);
     }
 
     @Test
     public void shouldIncreaseBackstagePassQualityBy3IfSellInDateLessThan5() {
-        assertQualitySuccess(4, new Item("Backstage passes to a TAFKAL80ETC concert", 1, 1));
+        this.items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 1, 1)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(4, app.items[0].quality);
     }
 
     @Test
     public void shouldIncreaseBackstagePassQualityBy2IfSellInLessThan10AndGreaterThan5() {
-        assertQualitySuccess(3, new Item("Backstage passes to a TAFKAL80ETC concert", 6, 1));
+        this.items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 6, 1)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(3, app.items[0].quality);
     }
 
     @Test
     public void shouldDisallowQualityGreaterThan50() {
-        assertQualitySuccess(50, new Item("Aged Brie", 1, 50));
+        this.items = new Item[]{new Item("Aged Brie", 1, 50)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(50, app.items[0].quality);
     }
 
     @Test
     public void shouldDisallowQualityLessThanZero() {
-        assertQualitySuccess(0, new Item("General Item", 1, 0));
+        this.items = new Item[]{new Item("General Item", 1, 0)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(0, app.items[0].quality);
     }
 
     @Test
     public void shouldSetBackstagePassQualityToZeroAfterConcert() {
-        assertQualitySuccess(0, new Item("Backstage passes to a TAFKAL80ETC concert", 0, 1));
+        this.items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 1)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(0, app.items[0].quality);
     }
 
     @Test
     public void shouldDecreaseSellinDateOnUpdate() {
-        assertSellInSuccess(1, new Item("General Item", 2, 1));
+        this.items = new Item[]{new Item("General Item", 2, 1)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(1, app.items[0].sellIn);
     }
 
     @Test
     public void shouldNotUpdateSulfurasSellinDate() {
-        assertSellInSuccess(1, new Item("Sulfuras, Hand of Ragnaros", 1, 1));
+        this.items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 1, 1)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(1, app.items[0].sellIn);
     }
 
     @Test
     public void shouldIncreaseBackstagePassQualityBy1IfSellInGreaterThan10() {
-        assertQualitySuccess(2, new Item("Backstage passes to a TAFKAL80ETC concert", 11, 1));
+        this.items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 11, 1)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(2, app.items[0].quality);
     }
 
     @Test
     public void shouldDecreaseGeneralItemQualityTwiceAsFastWhenSellInIsNegative() {
-        assertQualitySuccess(3, new Item("General Item", -1, 5));
+        this.items = new Item[]{new Item("General Item", -1, 5)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(3, app.items[0].quality);
     }
 
     @Test
     public void shouldIncreaseBackstagePassQualityBy3IfSellInIs5() {
-        assertQualitySuccess(4, new Item("Backstage passes to a TAFKAL80ETC concert", 5, 1));
+        this.items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 1)}
+        ;
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(4, app.items[0].quality);
     }
 
     @Test
     public void shouldDecreaseConjuredItemBy2OnUpdate() {
-        assertQualitySuccess(1, new Item("Conjured Item", 1, 3));
+        this.items = new Item[]{new Item("Conjured Item", 1, 3)};
+        this.app = new GildedRose(items);
+        this.app.updateQuality();
+        assertEquals(1, app.items[0].quality);
     }
 
 }
