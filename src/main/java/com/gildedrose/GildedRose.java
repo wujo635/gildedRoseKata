671fe7ded51f0d2c7cd19f230520531b7e929853
wujo <wujo635@gmail.com>
2019-03-14 23:25:58 -0400

    Refactor ItemUpdater selection

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,45 +2,34 @@ package com.gildedrose;
 
 import com.gildedrose.itemUpdater.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class GildedRose {
     Item[] items;
+    private static final Map<String, ItemUpdater> updaterMap = new HashMap<>();
+
+    static {
+        updaterMap.put("Aged Brie", new AgedBrieUpdater());
+        updaterMap.put("Backstage passes to a TAFKAL80ETC concert", new BackstagePassUpdater());
+        updaterMap.put("Sulfuras, Hand of Ragnaros", new SulfurasUpdater());
+        updaterMap.put("Conjured Item", new ConjuredItemUpdater());
+    }
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        ItemUpdater updater;
         for (Item item : items) {
-            if (isAgedBrie(item)) {
-                updater = new AgedBrieUpdater();
-            } else if (isBackstagePass(item)) {
-                updater = new BackstagePassUpdater();
-            } else if (isSulfuras(item)) {
-                updater = new SulfurasUpdater();
-            } else if (isConjuredItem(item)) {
-                updater = new ConjuredItemUpdater();
+            if (updaterMap.containsKey(item.name)) {
+                updaterMap.get(item.name).update(item);
             } else {
-                updater = new GenericItemUpdater();
+                ItemUpdater updater = new GenericItemUpdater();
+                updater.update(item);
             }
-            updater.update(item);
         }
-    }
-
-    private boolean isConjuredItem(Item item) {
-        return item.name.equals("Conjured Item");
-    }
-
-    private boolean isSulfuras(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    private boolean isBackstagePass(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
+        //TODO check all bounds of each special item
     }
 
 }
